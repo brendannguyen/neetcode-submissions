384e69b880fef58ad836b8efa5fc9853f335dee8
Brendan Nguyen <123909664+brendannguyen@users.noreply.github.com>
2026-05-30 20:38:46 +1000

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+
+        # compare time it takes to get to target
+        # go in reverse sorted order 
+        # if next car has < t (time to target), it will become fleet with front car, so we can remove
+        # same logic for next cars
+        # use stack, if car becomes fleet, pop
+        
+        cars = []
+        for i in range(len(position)):
+            cars.append((position[i], speed[i]))
+        # sorted based on position
+        cars.sort(key=lambda x:x[0])
+
+        stack = []
+        for i in range(len(cars)-1, -1, -1):
+            curr_time = (target - cars[i][0]) / cars[i][1]
+            if not stack or curr_time > stack[-1]:
+                stack.append(curr_time)
+        
+        return len(stack)
